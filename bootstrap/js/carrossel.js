f731bb4f26bc0217bd25a7c47254771f29b06380
TecoTecoChines <50380037+TecoTecoChines@users.noreply.github.com>
2019-12-11 00:47:42 -0300

    Modelo para um site de venda de roupas

---
+++ bootstrap/js/carrossel.js
@@ -0,0 +1,14 @@
+$(document).ready(function() {
+
+  $("#owl-demo").owlCarousel({
+
+      navigation : true,
+      slideSpeed : 400,
+      paginationSpeed : 400,
+      singleItem : true,
+	    autoPlay : 2000
+
+
+  });
+
+});
